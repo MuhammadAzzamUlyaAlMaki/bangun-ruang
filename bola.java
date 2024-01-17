31a9e1207e3d77747057e629297e3b0fb7845789
MuhammadAzzamUlyaAlMaki <azzam.310108@gmail.com>
2024-01-17 09:21:45 +0700

    first commit

---
+++ bola.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inheritance;
+
+/**
+ *
+ * @author SMK TELKOM 21
+ */
+public class bola extends BangunRuang{
+    float r;
+}
---
+++ tabung.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inheritance;
+
+/**
+ *
+ * @author SMK TELKOM 21
+ */
+public class tabung extends BangunRuang{
+    float tinggi;
+    float r;
+}
